f3323148d98849530e88961881d1fb540eb3ed98
dharapalanisamy206-creator <dharapalanisamy206@gmail.com>
2026-05-28 22:31:39 -0400

    Add files via upload

---
+++ setup_check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "=== SYSTEM INFO ==="
+echo "Hostname:$(hostname)"
+echo "User:$(whoami)"
+echo "IP Address:$ip addr showe | grep 'inet' |
+echo '{print $2}' | head -1)"
+echo "Kernal: $uname -r)"
+echo "===DISK USAGE==="
+df -h
